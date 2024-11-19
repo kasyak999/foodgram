22db48906a390ccd3059d0eb713e073fe0252ace
Максим <kasyak999@yandex.ru>
2024-11-19 13:43:20 +0300

    up

--- backend/s[_project/project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from api.views import short_link
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('s/<str:link>/', short_link, name='short-link'),
-]
