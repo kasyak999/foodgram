813f2685ed93965f1b5fe08ad122006b6906eab8
Максим <kasyak999@yandex.ru>
2024-11-19 23:40:36 +0300

    up

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+
 from .models import (
-    Basket, Favorite, Follow, Ingredient, Recipe, RecipeIngredient,
-    Teg, UserProfile)
+    Basket, Favorite, Follow, Ingredient,
+    Recipe, RecipeIngredient, Teg, UserProfile,
+)
 
 
 class UserProfileAdmin(admin.ModelAdmin):
--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+
 from .models import Basket, Favorite, Recipe, Teg
 
 
